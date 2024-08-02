a9a654b3c7e627f51637c7da9bb7d7bfd727117f
VictorLuanS <analistasilv@gmail.com>
2024-08-02 01:53:14 -0300

    codigo_teste.java

--- codigo_teste.java
+++ codigo_teste.java
@@ -2,7 +2,26 @@ package TreinamentoUnitarios._GitHub.main;
 
 public class codigo_teste {
     public static void main(String[] args) {
+        int A[] = {5,10,20,30,40};
+        int B[] = new int[4];
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                B[j] = A[i] * 2;
+                System.out.println(B[j]);
+
+                
+            }
+            
+        }
         
     }
     
 }
+
+
+/*
+ * Criar um vetor A com 5 elementos inteiros. Construir um vetor B de
+mesmo tipo e tamanho e com os elementos do vetor A multiplicados
+por 2, ou seja: B[i] = A[i] * 2.
+ */
